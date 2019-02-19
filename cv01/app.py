8d8812d8c6fd62ec0fe3bdc1aafa8dd987f48f67
Tomas "Jethro" Pokorny <xtompok@gmail.com>
2019-02-19 16:18:47 +0100

    Program z 1. setkani.

---
+++ cv01/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import json
+app = Flask(__name__)
+
+with open("static/DOP_PID_ZASTAVKY_B.json",encoding="utf-8") as zastfile:
+    zast = json.load(zastfile)
+    fs = zast["features"]
+
+@app.route("/")
+def hello():
+    return render_template("zastavky.html", features=fs)
+
+@app.route("/test")
+def test():
+    return ""
+
+app.run()
