8e9e5d4e8d0361cad1bff11d94d933bf34506c13
Tomas "Jethro" Pokorny <xtompok@gmail.com>
2019-04-09 16:20:47 +0200

    SW z 4. setkani.

--- cv01/app.py
+++ cv01/app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, abort, jsonify
 import json
 app = Flask(__name__)
 
 with open("static/DOP_PID_ZASTAVKY_B.json",encoding="utf-8") as zastfile:
     zast = json.load(zastfile)
     fs = zast["features"]
+
+lines = None
+linedict = {}
+with open("static/linky.json", encoding="utf-8") as linesfile:
+    lines = json.load(linesfile)
+    for f in lines["features"]:
+	    linedict[f["properties"]["LIN_ALIAS_WEB"]]=f
+
 
 @app.route("/")
 def hello():
@@ -17,5 +25,33 @@ def test():
 @app.route("/map")
 def mapPage():
     return render_template("map.html")
+
+def get_attribute(request,name,convert=None,default=None):
+    attr = request.args.get(name,None)
+    if not attr:
+        return (default,"Attribute '{}' undefined".format(name))
+    if convert:
+        try:
+            attr = convert(attr)
+        except:
+            return (default,"Attribute '{}' in wrong format".format(name))
+    return (attr,None)
+
+
+@app.route("/lines")
+def getLine():
+    (lname,error) = get_attribute(request,"name")
+    if not lname:
+	    abort(404)
+    
+    line = linedict.get(lname,None)
+    if not line:
+	    abort(404)
+	    # return "Line '{}' does not exist".format(lname)
+    res = {"type" : "FeatureCollection",
+    	   "features": [line]}
+    return jsonify(res)
+
+    
 
 app.run(port=5000,debug=True)
